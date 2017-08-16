57023169a8e344a05f66f915025f612c4343c684
sumitPhD <agrawalsumit1989@gmail.com>
2017-08-17 03:10:50 +0530

    code is correct but error due to large value of 'mu'(order>13)

--- J2.m
+++ J2.m
@@ -1,16 +1,58 @@
 %% Initial condition
 % time step
 t0 = 0;
-tf =1;
-% Initial position
-x10 =1;
-x20 =0;
-x30 = 2;
-% initial velocity
-v10 =0;
-v20 =0;
-v30 =0;
+tstep = 0.01;
+tf =100000;
+%%SGP 4 model's test case given in (test case is run in 360 minutes(6hr) of interval)
+%%http://www.celestrak.com/NORAD/documentation/spacetrk.pdf page 80
+%1 88888U 80275.98708465 .00073094 13844-3 66816-4 0 8
+%2 88888 72.8435 115.9689 0086731 52.6988 110.5714 16.05824518 105
+
+% taking initial position and velocity from the test case at t =0 and the
+% J2.m and J2fcn run for 1440 minutes and checked with the test data given
+% at 360 minutes of interval
+%input is given in meters and meters/sec
+
+
+% Orbital Parameters
+%[r,v] = orb2rv(p,e,i,O,o,nu,truLon,argLat,lonPer,mu)
+% Inputs: 
+%{
+p                                              %Semilatus Rectum (km)
+e                                             %Eccentricity Magnitude(unitless)
+i                                          % inclination (radians)
+O                                              %Right Ascention of the ascending node(radians)
+o                                           %Argument of perige% (radians)
+nu                                            %True Anomaly(radians)
+truLon                                        %True Longitude(radians)
+argLat                                        %Argument of Latitude(radians)
+lonPer                                        %Longitude of Periapse (radians)
+mu                                            %Gravitational Constant 
+%}
+%% Initial position (ref http://www.celestrak.com/NORAD/documentation/spacetrk.pdf)
+% x10 =-7.061909962477235e+06;
+% x20 =-5.167452270138207e+05;
+% x30 = -1.465993415673929e+04;
+% % initial velocity
+% v10 =-85.445362976044650;
+% v20 =1.064510485883730e+03;
+% v30 =7.427298812052643e+03;
+ Re = 6378164;
+% x10 = 2328.97*10^3; x20 =-5995.220*10^3; x30 = 1719.97*10^3;
+% v10 =2.912*10^3; v20=-0.9816*10^3; v30 =-7.09*10^3;
+mu = 6.673e-11*5.9742e24
+x10 = Re+100000; x20 =0; x30 = 0;
+v10 = 0; v20=sqrt(8*(10^12)/x10); v30 =0;
+% (v10^2+v20^2+v30^2)/sqrt(x10^2+x20^2+x30^2)
+% (v10*x10+v20*x20+v30*x30)/(sqrt(v10^2+v20^2+v30^2)*sqrt(x10^2+x20^2+x30^2))
 
 %% ode and plot
-[t,x] = ode45('J2fcn', [t0,tf], [x10 v10 x20 v20 x30 v30]);
-plot(t,x(:,1));
+[t,x] = ode45('J2fcn', [t0:tstep:tf], [x10 v10 x20 v20 x30 v30]);
+ 
+%J2_test_case = [[t0:tstep:tf]'; x(:,1); x(:,3);x(:,5);x(:,2);x(:,4);x(:,6)];
+rr = x(:,1).*x(:,1)+x(:,3).*x(:,3)+x(:,5).*x(:,5);
+plot(t, rr(:)/1000);
+%hold on 
+% rrsgp = SGP_120k(2,1:length(t)).*SGP_120k(2,1:length(t))+  SGP_120k(3,1:length(t)).*SGP_120k(3,1:length(t))+  SGP_120k(4,1:length(t)).*SGP_120k(4,1:length(t)); 
+% plot(t,rrsgp(:),'red');
+%save J2_test_case.mat 
--- J2fcn.m
+++ J2fcn.m
@@ -11,49 +11,44 @@ function xp = J2fcn( t,x )
 %[x1dot y1dot z1dot x2dot y2dot z2dot]'= [0 0 0 1 0 0 ;0 0 0 0 1 0; 0 0 0 0 0 1; Matrix] + [x1 y1 z1 x2 y2 z2];
 
 % initial condition as [postionX0  velocityXdot0 postionY0  velocityYdot0 postionZ0 velocityZdot0 ]
-
-%% constants
-% sidereal day is 23 hours, 56 minutes, 4.0916 seconds or 23.9344699 hours
+ut_sec = t; % universal time in sec
 stperut = 1.00273790935;    % siderial time = stperut * universal time  
-W_EARTH_ROT = 2*pi/(23.9344699*60*60);   % rotation angular velocity of earth, SI
-Re = 6378164 + 600000;    % Earth radius + Height of sat
+st_sec = stperut*ut_sec;    % sidereal time in sec
+W_EARTH_ROT = 2*pi/86400;
+phi = st_sec*W_EARTH_ROT;            % sidereal time in rad
+
+Re = 6378164;    % Earth radius + Height of sat
 mu=6.673e-11*5.9742e24;    % G*M_earth
-J2 = 1.08263*10^-3;   % dimensionless constant
-
-%%
-% ut_sec = (today - equinox)*24*60*60 + t; % universal time vector in sec
-% for the time being ut_sec = t
-ut_sec =t
-st_sec = stperut*ut_sec;    % sidereal time vector in sec
- 
- phi = st_sec*W_EARTH_ROT;            % sidereal time vector in rad
- 
-% TEI = [ cos(phi) sin(phi) 0;
-%        -sin(phi) cos(phi) 0;
-%         0        0        1];
-% TIE = TEI'    
-%%
-
-t
+ J2 = 0;%1.08263*10^-3;   % dimensionless constant
+t;
 xp = zeros(6,1);
-r = sqrt((x(1))^2+ (x(3))^2+ (x(5))^2 )
-ax =  -(mu/r^3)*(x(1))*(1+(3/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);
-ay = -(mu/r^3)*(x(3))*(1+(3/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);
-az =  -(mu/r^3)*(x(5))*(1+(9/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);
+r = sqrt((x(1))^2+ (x(3))^2+ (x(5))^2 );
+%ax =  -(mu/r^3)*(x(1))*(1+(3/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);
+%ay = -(mu/r^3)*(x(3))*(1+(3/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);
+%az =  -(mu/r^3)*(x(5))*(1+(9/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);
+%a= sqrt(ax^2+ay^2+az^2);
 % this is  in ECEF frame...have to convert in ECI frame 
+
+ax_n = -8*(10^12)*x(1)/(r^3);
+ay_n = -8*(10^12)*x(3)/(r^3);
+az_n = -8*(10^12)*x(5)/(r^3);
+
 xp(1) =  x(2);
-xp(2) = ax*cos(phi)-ay*sin(phi);
-
+xp(2) = ax_n;
 xp(3) =  x(4);
-xp(4) = ax*sin(phi)+ay*sin(phi);
+xp(4) = ay_n;
 xp(5) =  x(6);
-xp(6) = az;
+xp(6) = az_n;
+%xp-[x(2) ax x(4) ay x(6) az]'
 
+% xp(2) = ax*cos(phi)+ay*sin(phi);
+% xp(4) = -ax*sin(phi)+ay*cos(phi);
 
 %%
 %[xp(1), xp(3),xp(5)] = TIE*[xp(1) ,xp(3), xp(5)]';
 %[xp(2) xp(4) xp(6)]'= TIE*[xp(2) xp(4) xp(6)]';
 %[xp(1), xp(3),xp(5)]
+
 
 end
 
