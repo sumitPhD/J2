e99d8019d98aa1a0da471520c50db8f6be8d1fd1
Sanket Chirame <sanketchirame12@gmail.com>
2017-11-30 12:09:22 +0530

    optimized code for J2

--- J2fcn.m
+++ J2fcn.m
@@ -1,27 +1,27 @@
-function xp = J2fcn( t,x)
-
-% initial condition as [postionX0  velocityXdot0 postionY0  velocityYdot0 postionZ0 velocityZdot0 ]
-Re = 6378164;    % Earth radius + Height of sat
+function xdot = J2fcn( t,x)
+% x = [ x0 y0 z0 vx0 vy0 vz0 ]
+ Re = 6378164;    % Earth radius + Height of sat
 %mu=6.673e-11*5.9742e24;    % G*M_earth
-mu = 6.673e-11*5.9742e24;
+ mu = 6.673e-11*5.9742e24;
  J2 = 1.08263*10^-3;   % dimensionless constant
-t;
-xp = zeros(6,1);
- r = sqrt((x(1))^2+ (x(3))^2+ (x(5))^2 );  %4
-ax =  -(mu/r^3)*(x(1))*(1+(3/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);   %% 1 2(1 2 3)  =9   
-ay = -(mu/r^3)*(x(3))*(1+(3/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);
-az =  -(mu/r^3)*(x(5))*(1+(9/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(5))^2/r^4);
-%a= sqrt(ax^2+ay^2+az^2);
-% this is  in ECEF frame...have to convert in ECI frame 
-% 
-% ax_n = -mu*(1/(r^3))*x(1);
-% ay_n = -mu*(1/(r^3))*x(3);
-% az_n = -mu*(1/(r^3))*x(5);
-xp(1) =  x(2);   %% 6
-xp(2) = ax;
-xp(3) =  x(4);
-xp(4) = ay;
-xp(5) =  x(6);
-xp(6) = az;
+ xdot = zeros(6,1);
+ r = sqrt((x(1))^2+ (x(2))^2+ (x(3))^2 );  %4
+ r2 = r^2;
+ r3 = r2*r;
+ 
+ A1 = -mu/r3;
+ A2 = 1.5*J2*Re^2/r2;
+ A3 = 3*A2;
+ A4 = 5*A2*(x(3)^2)/r2;
+ 
+ xdot(4) = A1*x(1)*(1+A2-A4);
+ xdot(5) = A1*x(2)*(1+A2-A4);
+ xdot(6) = -A1*x(3)*(1+A3-A4);
+ xdot(1) = x(4);   
+ xdot(2) = x(5);
+ xdot(3) = x(6);
+% ax =  -(mu/r^3)*(x(1))*(1+(3/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(3))^2/r^4);   %% 1 2(1 2 3)  =9   
+% ay = -(mu/r^3)*(x(2))*(1+(3/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(3))^2/r^4);
+% az =  -(mu/r^3)*(x(3))*(1+(9/2)*J2*Re^2/r^2 - (15/2)*J2*Re^2*(x(3))^2/r^4);
 end
 
